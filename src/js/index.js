b7433a1bb7adae869da11332b2ff519f05fde0c2
itana <itanabulatovic.ib@gmail.com>
2019-10-04 10:24:05 +0200

    stage 1&2

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,23 @@
+let xpos=0;
+let opened=true;
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('it works');
+ 
+  document.addEventListener('keydown', (event)=>{
+    const pacman = document.querySelector('.entity');
+    
+    if(event.code === 'ArrowRight'){
+    
+      if (opened){
+         pacman.style.backgroundPositionX="0px";
+         opened=false;
+      }else{
+        pacman.style.backgroundPositionX="85px";
+        opened=true;
+      }
+      xpos+=85;
+      pacman.style.left=`${xpos}px`;   
+    }
+    
+  });
 });
