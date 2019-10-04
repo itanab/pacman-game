e12849687a7377ed044e7a4b2a8e8929c9ccff17
itana <itanabulatovic.ib@gmail.com>
2019-10-04 11:54:08 +0200

    mil2 stage 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,26 @@
-const jayne = new Pacman(0);
+const jayne = new Pacman(425, 425);
 
 const update = () => {
   const pacman = document.querySelector('.entity');
 
   pacman.style.left = `${jayne.xpos}px`;
+  pacman.style.top = `${jayne.ypos}px`;
+
 
   if (jayne.mouth) {
     pacman.style.backgroundPositionX = '0px';
   } else {
     pacman.style.backgroundPositionX = '85px';
+  }
+
+  if (jayne.dir === 'left') {
+    pacman.style.backgroundPositionY = '255px';
+  } else if (jayne.dir === 'up') {
+    pacman.style.backgroundPositionY = '85px';
+  } else if (jayne.dir === 'down') {
+    pacman.style.backgroundPositionY = '170px';
+  } else if (jayne.dir === 'right') {
+    pacman.style.backgroundPositionY = '0px';
   }
 };
 
@@ -17,7 +29,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
   document.addEventListener('keydown', (event) => {
     if (event.code === 'ArrowRight') {
-      jayne.moveRight();
+      jayne.move('right');
+      update();
+    } else if (event.code === 'ArrowLeft') {
+      jayne.move('left');
+      update();
+    } else if (event.code === 'ArrowUp') {
+      jayne.move('up');
+      update();
+    } else if (event.code === 'ArrowDown') {
+      jayne.move('down');
       update();
     }
   });
--- src/js/pacman.js
+++ src/js/pacman.js
@@ -1,11 +1,32 @@
 class Pacman {
-  constructor(xpos) {
+  constructor(xpos, ypos) {
     this.xpos = xpos;
+    this.ypos = ypos;
     this.mouth = true;
+    this.dir = 'right';
   }
 
-  moveRight() {
-    this.xpos += 85;
-    this.mouth = !this.mouth;
+  move(direction) {
+    this.dir = direction;
+    switch (direction) {
+      case 'right':
+        this.xpos += 85;
+        this.mouth = !this.mouth;
+        break;
+      case 'left':
+        this.xpos -= 85;
+        this.mouth = !this.mouth;
+        break;
+      case 'up':
+        this.ypos -= 85;
+        this.mouth = !this.mouth;
+        break;
+      case 'down':
+        this.ypos += 85;
+        this.mouth = !this.mouth;
+        break;
+
+      default:
+    }
   }
 }
