2d84fbad9ca6c099a0097ec35d67650260b0b1b2
itana <itanabulatovic.ib@gmail.com>
2019-10-04 15:55:22 +0200

    Milstone 3 Walls

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.container');
-  const stage = new Stage(10, 10);
+  const stage = new Stage(6, 10);
   stage.mount(container);
 
   const jayne = new Pacman(0, 3, stage);
   jayne.mount(stage.element);
 
 
-  const wall2 = new Entity(0, 4, 'wall');
-  wall2.mount(stage);
-
-  const wall = new Entity(5, 8, 'wall');
+  const wall = new Entity(0, 4, 'wall');
   wall.mount(stage);
 
-  const wall3 = new Entity(8, 9, 'wall');
+  const wall1 = new Entity(0, 5, 'wall');
+  wall1.mount(stage);
+
+  const wall2 = new Entity(1, 5, 'wall');
+  wall2.mount(stage);
+
+  const wall5 = new Entity(1, 6, 'wall');
+  wall5.mount(stage);
+
+  const wall6 = new Entity(2, 6, 'wall');
+  wall6.mount(stage);
+
+  const wall3 = new Entity(5, 8, 'wall');
   wall3.mount(stage);
+
+  const wall4 = new Entity(5, 9, 'wall');
+  wall4.mount(stage);
+
+  const wall7 = new Entity(4, 0, 'wall');
+  wall7.mount(stage);
+
+  const apple = new Entity(4, 3, 'apple');
+  apple.mount(stage);
 
   document.addEventListener('keydown', (event) => {
     if (event.code === 'ArrowRight') {
