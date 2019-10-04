9723a88de3678dfb7ff7c552800e5b7818eb8727
itana <itanabulatovic.ib@gmail.com>
2019-10-04 12:22:39 +0200

    mil2 stage 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,21 @@
-const jayne = new Pacman(425, 425);
-
-const update = () => {
-  const pacman = document.querySelector('.entity');
-
-  pacman.style.left = `${jayne.xpos}px`;
-  pacman.style.top = `${jayne.ypos}px`;
-
-
-  if (jayne.mouth) {
-    pacman.style.backgroundPositionX = '0px';
-  } else {
-    pacman.style.backgroundPositionX = '85px';
-  }
-
-  if (jayne.dir === 'left') {
-    pacman.style.backgroundPositionY = '255px';
-  } else if (jayne.dir === 'up') {
-    pacman.style.backgroundPositionY = '85px';
-  } else if (jayne.dir === 'down') {
-    pacman.style.backgroundPositionY = '170px';
-  } else if (jayne.dir === 'right') {
-    pacman.style.backgroundPositionY = '0px';
-  }
-};
-
 document.addEventListener('DOMContentLoaded', () => {
-  update();
+  const stage = document.querySelector('#stage');
+  const jayne = new Pacman(425, 425);
+  jayne.mount(stage);
 
   document.addEventListener('keydown', (event) => {
     if (event.code === 'ArrowRight') {
       jayne.move('right');
-      update();
+      jayne.update();
     } else if (event.code === 'ArrowLeft') {
       jayne.move('left');
-      update();
+      jayne.update();
     } else if (event.code === 'ArrowUp') {
       jayne.move('up');
-      update();
+      jayne.update();
     } else if (event.code === 'ArrowDown') {
       jayne.move('down');
-      update();
+      jayne.update();
     }
   });
 });
--- src/js/pacman.js
+++ src/js/pacman.js
@@ -1,3 +1,4 @@
+
 class Pacman {
   constructor(xpos, ypos) {
     this.xpos = xpos;
@@ -27,6 +28,43 @@ class Pacman {
         break;
 
       default:
+    }
+  }
+
+  render() {
+    this.element = document.createElement('div');
+    this.element.className = 'container';
+    this.element.innerHTML = (
+      '<div class="entity entity--pac pacgirl-active-light">Jayne</div>'
+    );
+  }
+
+  mount(parent) {
+    this.render();
+    parent.appendChild(this.element);
+  }
+
+  update() {
+    const pacman = this.element.querySelector('.entity');
+
+    pacman.style.left = `${this.xpos}px`;
+    pacman.style.top = `${this.ypos}px`;
+
+
+    if (this.mouth) {
+      pacman.style.backgroundPositionX = '0px';
+    } else {
+      pacman.style.backgroundPositionX = '85px';
+    }
+
+    if (this.dir === 'left') {
+      pacman.style.backgroundPositionY = '255px';
+    } else if (this.dir === 'up') {
+      pacman.style.backgroundPositionY = '85px';
+    } else if (this.dir === 'down') {
+      pacman.style.backgroundPositionY = '170px';
+    } else if (this.dir === 'right') {
+      pacman.style.backgroundPositionY = '0px';
     }
   }
 }
